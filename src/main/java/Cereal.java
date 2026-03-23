82febd40afa546529c76dfe17c13cc74370d1a50
Taylor Belcher <30426680+hotdogontology@users.noreply.github.com>
2026-03-23 14:48:39 -0400

    Update Cereal.java

--- src/main/java/Cereal.java
+++ src/main/java/Cereal.java
@@ -1,22 +1,64 @@
 /**
- * Cereal.java
+ * Cereal.java  
  *
  * @author – Your name
- * @author – Class period
- *
+ * @author – Class period 
+ * 
  */
 public class Cereal
 {
-    // private instance variables to hold info about a cereal from the data set
-    // the name, calories, fiber, carbohydrates, and cups
+   private String name; 
+   private int calories; 
+   private double fiber, carbs, cups; 
 
-    // constructor that creates a cereal object
+   public Cereal(String initName, int initCal, double initFiber, 
+                 double initCarbs,  double initCups)
+   {
+      name = initName; 
+      calories = initCal; 
+      fiber = initFiber; 
+      carbs = initCarbs;
+      cups = initCups; 
+   }
+   /* Accessor method returns the value stored in the instance variable name. */
+   public String getName()
+   {
+      return name;
+   }
 
-    // accessor methods for each instance variable
+   /* Accessor method returns the value stored in the instance variable carbs. */      
+   public double getCarbs()
+   {
+      return carbs;
+   }
 
-    /* toString method returns the values stored in the instance variables
-       concatenated in a sentence starting with "Cereal: " */
+   /* Accessor method returns the value stored in the instance variable cups. */       
+   public double getCups()
+   { 
+      return cups;
+   }
 
+   /* Accessor method returns the value stored in the instance variable fiber. */
+   public double getFiber() 
+   { 
+      return fiber;
+   }
+
+   /* Accessor method returns the value stored in the instance variable calories.  
+    */
+   public int getCalories()
+   {
+      return calories;
+   }
+
+
+   /* toString method returns the values stored in the instance variables   
+      concatenated in a sentence. */
+   public String toString()
+   {
+      return "Cereal: " + name + " has: " + calories 
+              + "calories, in a " + cups + " cup serving, with "
+              + fiber + "grams of fiber and "
+              + carbs +"grams of carbohydrates";
+   }
 }
-
-
